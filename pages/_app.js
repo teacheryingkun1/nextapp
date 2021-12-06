4248d4095f760f249d4d8a2bdc491a01ba04ecb1
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-06 21:53:14 +0700

    v4

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import Head from 'next/head'
 import Nav from '../components/Nav'
 import Foot from '../components/Foot'
 import "../styles/index.css"
+import Script from 'next/script'
+
+
 
 export default function app({ Component, pageProps }) {
     return (
@@ -11,11 +14,12 @@ export default function app({ Component, pageProps }) {
             <Head>
                 <title>Myapp</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                
             </Head>
             <Nav />
             <div className="index-content">
                 <Component {...pageProps} />
+                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>
             </div>
             <Foot />
         </div>
